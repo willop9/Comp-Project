2aaace84aeeabbd7811129824ff92a51c357636a
William <willop9@googlemail.com>
2019-03-11 13:44:44 +0000

    Graph import and init

--- graph.py
+++ graph.py
@@ -1,8 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
-#
-site = urllib.request.urlopen("file:./docs/page.html")
+import networkx as nx
 
+G = nx.Graph()
+site = urllib.request.urlopen("file:./docs/page.html")
 soup = BeautifulSoup(site, 'lxml')
 
 for child in soup.descendants:
