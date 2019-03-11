b4366929f039fe9a6f446bdd83f6330811635153
William <willop9@googlemail.com>
2019-03-11 15:28:22 +0000

    Added second graph and verify that all nodes have been correctly added

--- graph.py
+++ graph.py
@@ -3,9 +3,34 @@ from bs4 import BeautifulSoup
 import networkx as nx
 
 G = nx.Graph()
+H = nx.Graph()
+
 site = urllib.request.urlopen("file:./docs/page.html")
+bbc = urllib.request.urlopen("https://www.bbc.co.uk")
+
 soup = BeautifulSoup(site, 'lxml')
+soup2 = BeautifulSoup(bbc, 'lxml')
+
+soupNodes = []
+bbcNodes = []
 
 for child in soup.descendants:
     if(child.name is not None):
-        print(child)
+        print(child.name)
+        soupNodes.append(child)
+
+for child in soup2.descendants:
+    if(child.name is not None):
+        bbcNodes.append(child)
+
+G.add_nodes_from(soupNodes)
+H.add_nodes_from(bbcNodes)
+
+print(G.number_of_nodes()) #15 is the correct number!
+print(H.number_of_nodes()) #1344
+
+nodes = list(G.nodes)
+for x in nodes:
+    print('//////////////////////////')
+    print(x)
+    print('#########################')
