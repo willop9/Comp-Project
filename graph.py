e7bec4372fdc98900e0b478bc72679f51db8f092
William <willop9@googlemail.com>
2019-04-02 13:56:27 +0100

    Added Iframe edge and HTTP type attribute

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 G = nx.Graph()
 H = nx.Graph()
 
-site = urllib.request.urlopen("file:./docs/page.html")
+site = urllib.request.urlopen("https://www.bbc.co.uk")#file:./docs/page.html
 bbc = urllib.request.urlopen("https://www.bbc.co.uk")
 
 soup = BeautifulSoup(site, 'lxml')
@@ -16,6 +16,7 @@ HTMLNodes = []
 httpNodes = []
 htmlToHtmlEdges = []
 htmlToHttpEdges = []
+htmlToHttpIframe = []
 bbcNodes = []
 #Adding all page elements to node list
 for child in soup.descendants:
@@ -47,14 +48,21 @@ for n in list(G.nodes):
                                 G.add_edge(n,m)
 
 print(G.number_of_edges())
-
+#Adding HTTP layer
 for n in soup.find_all(src=True):
         src = n['src']
         print(n)
-        G.add_node(src, type='HTTP')
+        G.add_node(src, type='HTTP source')
         httpNodes.append(src)
         htmlToHttpEdges.append([n,src])
         G.add_edge(n,src)
+
+for n in soup.find_all('iframe'):
+        src = n['src']
+        G.nodes[src]['type'] = 'HTTP iframe'
+        G.add_edge(n.parent,src)
+        htmlToHttpIframe.append([n.parent,src])
+
 
 print(G.number_of_nodes())#19?????
 #drawing graph fingers crossed
@@ -64,7 +72,7 @@ nx.draw_networkx_nodes(G,pos,
                        nodelist=HTMLNodes,
                        node_color='r',
                        node_size=300,
-                   alpha=1)
+                       alpha=1)
 nx.draw_networkx_edges(G,pos,
                        edgelist=htmlToHtmlEdges,
                        width=3,alpha=0.8,edge_color='black')
@@ -72,10 +80,14 @@ nx.draw_networkx_nodes(G,pos,
                        nodelist=httpNodes,
                        node_color='b',
                        node_size=300,
-                   alpha=1)
+                       alpha=1)
 nx.draw_networkx_edges(G,pos,
-                       edgelist=htmlToHttpEdges,
-                       width=3,alpha=0.8,edge_color='green')
+                      edgelist=htmlToHttpEdges,
+                      width=3,alpha=0.8,edge_color='green')
+nx.draw_networkx_edges(G,pos,
+                       edgelist=htmlToHttpIframe,
+                       width=3,alpha=0.8,edge_color='blue')
+
 plt.axis('off')     
 plt.show()
 #Things to do next:
@@ -88,3 +100,5 @@ plt.show()
 #Add edges not by adding them to a list but individually as well
 #Add nodes independantly and give them atrribute of misc
 #find all img, style, and iframe nodes and give them attributes
+
+#HTML to HTML edges need no attributes
