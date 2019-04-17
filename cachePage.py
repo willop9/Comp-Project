9f507682875d8b5cb1acbc040047cf51aed6755d
Will Roberts <willop9@googlemail.com>
2019-04-17 11:26:51 +0100

    Fixed encoding issues

--- cachePage.py
+++ cachePage.py
@@ -1,12 +1,14 @@
 import urllib.request
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 driver = webdriver.Firefox()
-url='https://www.theguardian.com/uk'
+url='https://www.bristol-sport.co.uk/'
 driver.get(url)
+time.sleep(10)
 html = driver.page_source
 soup = BeautifulSoup(html, 'lxml')
-name = 'guardian'
-#soup.prettify()
-with open(name + '.html', "w") as file:
+name = 'bristolsport'
+soup.prettify()
+with open('./pages/'+ name + '.html', "w", encoding="utf-8") as file:
     file.write(str(soup))
